961cf32ddf507932cce68ccaf8f6307a8fc6577c
shenzhaonan <>
2020-01-08 20:20:10 +0800

    rm no use package

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,6 +6,9 @@ public class Test : MonoBehaviour
 {
     void Awake()
     {
+        AudioConfig.UpdateProgressAction += (InF, InS) => Debug.Log(InF + "<>" + InS);
+        AudioConfig.UpdateCompletedCallbackAction += Debug.Log;
+        
         UnityGuiTest.UnityGUIAction += () =>
         {
             if (GUILayout.Button("Piano"))
@@ -15,12 +18,26 @@ public class Test : MonoBehaviour
 
             if (GUILayout.Button("Piano2_6ra"))
             {
-                AudioManager.Instance.PlayEffect(AudioKey.Piano2_6ra);
+                AudioManager.Instance.PlayEffect(AudioKey.Piano26);
             }
 
             if (GUILayout.Button("Piano2_6si"))
             {
-                AudioManager.Instance.PlayEffect(AudioKey.Piano2_7si);
+                AudioManager.Instance.PlayEffect(AudioKey.Piano27);
+            }
+            if (GUILayout.Button("bird02"))
+            {
+                AudioManager.Instance.PlayEffect(AudioKey.Bird02);
+            }
+            
+            if (GUILayout.Button("bird03"))
+            {
+                AudioManager.Instance.PlayEffect(AudioKey.Bird03);
+            }
+            
+            if (GUILayout.Button("bird05"))
+            {
+                AudioManager.Instance.PlayEffect(AudioKey.Bird05);
             }
         };
     }
